1fbcfc95465f20b4964b8e229fe11d4d92c5a73d
Sang Yoon <sangyun0904@gmail.com>
2022-04-28 15:35:29 +0900

    Add baekjoon 2565 and Solve 11054

--- baekjoon/DynamicProgramming/baekjoon11054.py
+++ baekjoon/DynamicProgramming/baekjoon11054.py
@@ -9,33 +9,23 @@ Created on Thu Apr 21 12:44:29 2022
 import sys 
 input = sys.stdin.readline 
 
-_ = int(input())
+N = int(input())
 A = list(map(int, input().split()))
 
-def getDescLen(A, idx):
-    memo = [[] for _ in range(idx, len(A))]
-    memo[0].append(A[idx])
-    cnt = 1
-    for i in range(idx, len(A)):
-        for j in memo:
-            if j != []:
-                if A[i] > memo[cnt-1][-1] and A[i] < A[idx]:
-                    for m in memo[0]:
-                        if m > A[i]:
-                            memo[cnt].append(m)
-                    cnt += 1
-                if A[i] < j[-1]:
-                    j.append(A[i])
-    return max(memo, key=lambda x:len(x))
-    
-answers = []   
- 
-for i in range(len(A)):
-    l1 = getDescLen(A, i)
-    A.reverse()
-    l2 = getDescLen(A, len(A)-i-1)
-    A.reverse()
-    
-    answers.append(len(l1) + len(l2) - 1)
+asc = [1 for i in range(N)]
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            asc[i] = max(asc[i], asc[j]+1)
+            
+desc = [1 for i in range(N)]
+for i in range(N-1, -1, -1):
+    for j in range(N-1, i-1, -1):
+        if A[i] > A[j]:
+            desc[i] = max(desc[i], desc[j]+1)
+            
+ans = [0 for i in range(N)]
 
-print(max(answers))
+for i in range(N):
+    ans[i] = asc[i] + desc[i] -1 
+print(max(ans))
---
+++ baekjoon/DynamicProgramming/baekjoon2565.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 13:29:29 2022
+
+@author: USER
+"""
+from itertools import combinations
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+lines = [list(map(int, input().split())) for _ in range(N)]
+lines.sort(key=lambda x:x[0])
+memo = [1 for _ in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if lines[i][1] > lines[j][1]:
+            memo[i] = max(memo[i], memo[j]+1)
+            
+print(N - max(memo))
