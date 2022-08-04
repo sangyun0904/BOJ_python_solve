fb7b8d5e35f5ba331803d029f03a680dfded4bbb
sangyoon kim <sangyun0904@gmail.com>
2022-08-04 15:42:22 +0900

    Modify baekjoon1010

--- baekjoon/DynamicProgramming/baekjoon1010.py
+++ baekjoon/DynamicProgramming/baekjoon1010.py
@@ -19,4 +19,7 @@ for i in range(1, maxInt):
         elif i < j:
             dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
 
-print(dp[13][29])
+T = int(input())
+for _ in range(T):
+    N, M = map(int, input().split())
+    print(dp[N][M])
