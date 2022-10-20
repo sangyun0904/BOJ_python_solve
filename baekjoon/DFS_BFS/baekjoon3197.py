d72caa8767bc833f1c2d9f23fbb7952afc94a56f
sangyoon kim <sangyun0904@gmail.com>
2022-10-20 14:21:08 +0900

    Solve baekjoon 3197 bfs

--- baekjoon/DFS_BFS/baekjoon3197.py
+++ baekjoon/DFS_BFS/baekjoon3197.py
@@ -11,11 +11,8 @@ from collections import deque
 
 input = sys.stdin.readline 
 
-def bfs(start):
-    q = deque([start])
-    
-    visited = [[False for _ in range(C)] for _ in range(R)]
-    visited[start[0]][start[1]] = True
+def find_swan(q):
+    next_q = deque()
     
     while q:
         x, y = q.popleft()
@@ -25,69 +22,84 @@ def bfs(start):
             yy = y + dy[i]
             
             if 0 <= xx < R and 0 <= yy < C:
-                if not visited[xx][yy] and graph[xx][yy] != 'X':
-                    if graph[xx][yy] == 'L':
-                        return True
-                    q.append([xx, yy])
+                if not visited[xx][yy]: 
+                    if graph[xx][yy] != 'X':
+                        if graph[xx][yy] == 'L':
+                            return True, q
+                        q.append([xx, yy])
+                    else:
+                        next_q.append([xx, yy])
                     visited[xx][yy] = True
                     
-    return False
+                    
+    return False, next_q
+
+
+def melt(water):
+    R = len(graph)
+    C = len(graph[0])
+    next_water = deque() 
+    
+    while water:
+        cur = water.popleft() 
+        x = cur[0]
+        y = cur[1]
+        
+        for i in range(4):
+            xx = x + dx[i]
+            yy = y + dy[i]
+            
+            if 0 <= xx < R and 0 <= yy < C:
+                if graph[xx][yy] == "X":
+                    graph[xx][yy] = "."
+                    next_water.append([xx, yy])
+                    
+    return next_water
+
+
+def start():
+    for i in range(R):
+        for j in range(C):
+            if graph[i][j] == 'L':
+                visited[i][j] = True 
+                return deque([[i, j]])
+        
 
                     
 
 R, C = map(int, input().split())
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+visited = [[False for _ in range(C)] for _ in range(R)]
 
 graph = []
 
 for _ in range(R):
-    graph.append(input().strip())
+    graph.append(list(input().strip()))
     
-for i in range(R):
-    for j in range(C):
-        if graph[i][j] == 'L':
-            swan = [i, j]
-    
+q = start()
+
 time = 0
 
-melt = []
+water = deque()
 
 for i in range(R):
     for j in range(C):
-        if graph[i][j] == 'X':
-            for k in range(4):
-                xx = i + dx[k]
-                yy = j + dy[k]
-                
-                if 0 <= xx < R and 0 <= yy < C:
-                    if graph[xx][yy] == '.':
-                        melt.append([i,j])
-                        break
+        if graph[i][j] == '.' or graph[i][j] == 'L':
+            water.append([i,j])
     
 while True:
-    if bfs(swan):
+    
+    print(q)
+    
+    meet, q = find_swan(q)
+    
+    if meet:
         print(time)
         break 
     
                         
-    for i, j in melt:
-        graph[i] = graph[i][:j] + '.' + graph[i][j+1:]
-        
-    melt_next = []
-    
-    for _ in range(len(melt)):
-        i, j = melt.pop()
-        for k in range(4):
-            xx = i + dx[k]
-            yy = j + dy[k]
-            
-            if 0 <= xx < R and 0 <= yy < C:
-                if graph[xx][yy] == 'X':
-                    melt_next.append([xx, yy])
-    
-    melt = melt_next
-                
+    water = melt(water)
     for i in graph:
         print(i)
     print()
