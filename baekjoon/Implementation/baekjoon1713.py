702bcdf0a7f7c0fc4bfbe1583e7532f4049874bd
Sang Yoon <sangyun0904@gmail.com>
2023-01-28 22:47:01 +0900

    Solve baekjoon10971 backtracking

--- baekjoon/Implementation/baekjoon1713.py
+++ baekjoon/Implementation/baekjoon1713.py
@@ -9,16 +9,16 @@ for i in recommend:
     for j in range(len(candid)):
         n, count, time = candid[j] 
         if i == n:
-            candid[j][1] -= 1 
+            candid[j][1] += 1 
             isCandid = True
         candid[j][2] += 1
     
     if not isCandid:
         if len(candid) == N:
             candid.pop()
-        candid.append([i, -1, 0])
+        candid.append([i, 1, 0])
     
-    candid.sort(key=lambda x : [x[1], x[2]])
+    candid.sort(key=lambda x : [-x[1], x[2]])
 
 answer = []
 
